64a333434e1d54a65ef068dcd0193a46b54ab17c
chinedu umeh <chineduumeh@chinedus-MacBook-Pro.local>
2026-03-24 23:54:24 +0100

    initial-commit

---
+++ src/app/about/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text,SafeAreaView} from 'react-native'
+import React from 'react'
+const index = () => {
+  return (
+    <SafeAreaView>
+    <View>
+      <Text>AboutScreen</Text>
+    </View>
+    </SafeAreaView>
+  )
+}
+
+export default index
